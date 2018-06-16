6d425f86e4d7b2ec6eaef4dc222a41eb76dc6413
Yogesh Naik <yogeshrnaik@gmail.com>
2018-06-16 19:18:11 +0530

    code

--- linkedin/Ex_Files_Learning_Python/Exercise Files/Ch2/classes_start.py
+++ linkedin/Ex_Files_Learning_Python/Exercise Files/Ch2/classes_start.py
@@ -1,10 +1,31 @@
 #
 # Example file for working with classes
 #
+class MyClass():
+  def method1(self):
+    print("MyClass method1")
+
+  def method2(self, someString):
+    print("MyClass method2 : " + someString)
+
+
+class AnotherClass(MyClass):
+  def method1(self):
+    MyClass.method1(self)
+    print("AnotherClass method1")
+
+  def method2(self, someString):
+    print("AnotherClass method2")
 
 
 def main():
-  print("main")
+  o = MyClass()
+  o.method1()
+  o.method2("This is some string")
+
+  a = AnotherClass()
+  a.method1()
+  a.method2("This is another string")
   
 if __name__ == "__main__":
   main()
