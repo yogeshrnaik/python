e790b63949832f7da4d8f3289fe579d78db8aec7
Yogesh Naik <yogeshrnaik@gmail.com>
2018-06-19 14:03:18 +0530

    HTML and XML parsing

--- linkedin/Ex_Files_Learning_Python/Exercise Files/Ch5/xmlparsing_start.py
+++ linkedin/Ex_Files_Learning_Python/Exercise Files/Ch5/xmlparsing_start.py
@@ -1,22 +1,31 @@
 # 
 # Example file for parsing and processing XML
 #
-
+import xml.dom.minidom
 
 def main():
   # use the parse() function to load and parse an XML file
-
+  doc = xml.dom.minidom.parse('./Ch5/samplexml.xml')
   
   # print out the document node and the name of the first child tag
-
+  print(doc.nodeName)
+  print(doc.firstChild.tagName)
   
   # get a list of XML tags from the document and print each one
-
+  printSkills(doc)
     
   # create a new XML tag and add it into the document
+  newSkill = doc.createElement("skill")
+  newSkill.setAttribute("name", "jQuery")
+  doc.firstChild.appendChild(newSkill)
+  printSkills(doc)
 
+def printSkills(doc):
+  skills = doc.getElementsByTagName('skill')
+  print("%d skills:" %skills.length)
+  for s in skills:
+    print(s.getAttribute('name'))
   
-
 if __name__ == "__main__":
-  main();
+  main()
 
