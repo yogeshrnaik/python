ed7877928b137cff83b81947b293e60f8c25ddd6
Yogesh Naik <yogeshrnaik@gmail.com>
2018-06-19 10:00:05 +0530

    code

--- linkedin/Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
+++ linkedin/Ex_Files_Learning_Python/Exercise Files/Ch3/calendars_start.py
@@ -3,24 +3,43 @@
 #
 
 # import the calendar module
-
+import calendar
 
 # create a plain text calendar
-
+c = calendar.TextCalendar(calendar.MONDAY)
+st = c.formatmonth(2018, 6, 0, 0)
+print(st)
 
 # create an HTML formatted calendar
-
+hc = calendar.HTMLCalendar(calendar.MONDAY)
+st = hc.formatmonth(2018, 6)
+print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+for i in c.itermonthdays(2018, 6):
+    print(i)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+for name in calendar.month_name:
+    print(name)
 
+for day in calendar.day_name:
+    print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
-  
+print("Team meetings will be on: ")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(2018, m)
+    # print(cal)
+    week1 = cal[0]
+    week2 = cal[1]
+    if week1[calendar.FRIDAY] != 0:
+        meetday = week1[calendar.FRIDAY]
+    else:
+        meetday = week2[calendar.FRIDAY]
+    print("%10s %2d" % (calendar.month_name[m], meetday))
--- linkedin/Ex_Files_Learning_Python/Exercise Files/Ch4/files_start.py
+++ linkedin/Ex_Files_Learning_Python/Exercise Files/Ch4/files_start.py
@@ -4,19 +4,27 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
-
+  #f = open('textfile.txt', 'w+')
 
   # Open the file for appending text to the end
-
+  #f = open('textfile.txt', 'a')
 
   # write some lines of data to the file
-
+  #for i in range(10):
+  #  f.write("This is line %d\n" % (i+1))
   
   # close the file when done
+  #f.close()
 
-  
   # Open the file back up and read the contents
-
+  f = open('textfile.txt', 'r')
+  if (f.mode == 'r'):
+    # contents = f.read()
+    # print(contents)
+    fl = f.readlines()
+    for line in fl:
+      print(line)
+    
     
 if __name__ == "__main__":
   main()
