5dadbc462b6ba833f49930cc8c78543028ad11b4
Yogesh Naik <yogeshrnaik@gmail.com>
2018-06-24 23:26:57 +0530

    for and while

--- linkedin/Ex_Files_Python_EssT/Exercise Files/Chap06/for.py
+++ linkedin/Ex_Files_Python_EssT/Exercise Files/Chap06/for.py
@@ -4,5 +4,9 @@
 animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
 
 for pet in animals:
+    # if pet == 'dog': continue
+    # if pet == 'velociraptor': break
     print(pet)
+else:
+    print('that is all the animals')
 
--- linkedin/Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
+++ linkedin/Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
@@ -3,6 +3,18 @@
 
 secret = 'swordfish'
 pw = ''
+auth = False
+count = 0
+max_attempts = 5
+
 
 while pw != secret:
-    pw = input("What's the secret word? ")
+    count += 1
+    if count > max_attempts: break
+    if count == 3: continue
+    pw = input(f"{count}: What's the secret word? ")
+else: # else clause gets executed when while condition is no longer true i.e. loops exits normally
+    print('Correct password was entered...')
+    auth = True
+
+print("Authorized" if auth else "Not authorized")
