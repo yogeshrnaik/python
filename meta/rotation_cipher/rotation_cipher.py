b61a1012d43500a9c563110896b898d2200831d6
Yogesh Naik <yogeshrnaik@gmail.com>
2022-05-03 11:41:11 +0530

    Meta: Balance brackets

--- meta/rotation_cipher/rotation_cipher.py
+++ meta/rotation_cipher/rotation_cipher.py
@@ -1,10 +1,42 @@
 import math
 
+'''
+Rotational Cipher
+One simple way to encrypt a string is to "rotate" every alphanumeric character by a certain amount. Rotating a character means replacing it with another character that is a certain number of steps away in normal alphabetic or numerical order.
+For example, if the string "Zebra-493?" is rotated 3 places, the resulting string is "Cheud-726?". Every alphabetic character is replaced with the character 3 letters higher (wrapping around from Z to A), and every numeric character replaced with the character 3 digits higher (wrapping around from 9 to 0). Note that the non-alphanumeric characters remain unchanged.
+Given a string and a rotation factor, return an encrypted string.
+Signature
+string rotationalCipher(string input, int rotationFactor)
+Input
+1 <= |input| <= 1,000,000
+0 <= rotationFactor <= 1,000,000
+Output
+Return the result of rotating input a number of times equal to rotationFactor.
+Example 1
+input = Zebra-493?
+rotationFactor = 3
+output = Cheud-726?
+Example 2
+input = abcdefghijklmNOPQRSTUVWXYZ0123456789
+rotationFactor = 39
+output = nopqrstuvwxyzABCDEFGHIJKLM9012345678
+'''
+
 
 # Add any extra import statements you may need here
 
 
 # Add any helper functions you may need here
+def isLowercaseAlphabet(c):
+    return c in "abcdefghijklmnopqrstuvwxyz"
+
+
+def isUppercaseAlphabet(c):
+    return c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def isNumber(c):
+    return c in "0123456789"
 
 
 def rotationalCipher(input, rotation_factor):
@@ -23,18 +55,6 @@ def rotationalCipher(input, rotation_factor):
         else:
             result += c
     return result
-
-
-def isLowercaseAlphabet(c):
-    return c in "abcdefghijklmnopqrstuvwxyz"
-
-
-def isUppercaseAlphabet(c):
-    return c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
-def isNumber(c):
-    return c in "0123456789"
 
 
 # These are the tests we use to determine if the solution is correct.
